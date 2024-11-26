bd518158d59984af71bd63442c0f6830dedb1901
Yelyzaveta <yelyzavetamatskiv09@gmail.com>
2024-11-26 21:17:35 +0200

    Add files via upload

---
+++ lab_06/calculator/script.js
@@ -0,0 +1,22 @@
+let firstNumber = Number(prompt("Enter the first number"));
+let mathSign = prompt("Enter the math sign");
+let secondNumber = Number(prompt("Enter the first number"));
+
+if (mathSign== '+') {
+    alert(firstNumber + secondNumber);
+}
+
+else if (mathSign== '-') {
+    alert(firstNumber - secondNumber);
+}
+
+else if (mathSign== '*') {
+    alert(firstNumber * secondNumber);
+}
+
+else if (mathSign== '/') {
+    alert(firstNumber / secondNumber);
+}
+
+if (mathSign == any);
+
