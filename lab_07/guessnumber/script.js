b638181a2f58c3ac58d9dc226777c08e1b6bfac6
Yelyzaveta <yelyzavetamatskiv09@gmail.com>
2024-12-09 12:47:43 +0200

    Add files via upload

---
+++ lab_07/guessnumber/script.js
@@ -0,0 +1,43 @@
+function startGame() {
+    if(!confirm("Start new game\nCurrent balance: 100$")) {
+        alert ("Game cancelled")
+        return;
+    }
+}
+
+function askToRestartGame() {
+    let restart = confirm("Start new game?");
+    if (restart) {
+         startGameLoop();
+    } else {
+        alert("Game cancelled");
+    }
+}
+
+function startGameLoop() {
+    let balance = 100;
+    let randomNumber = Math.floor(Math.random() * 6);
+    console.log("Random Number:", randomNumber);
+
+    for (let attempt = 0; attempt < 3; attempt++) {
+    let givenNumber = prompt(`Guess the Number\nCurrent balance: ${balance}$`);
+    console.log("Given Number:", givenNumber);
+
+    if (parseInt(givenNumber) === randomNumber) {
+        let reward = (3 - attempt) * 10 + 20; 
+        balance += reward;
+        alert(`Wow you won! Your reward - ${reward}$. Updated balance: ${balance}$`);
+        break;
+
+    } else {
+        balance -= 10;
+        if (attempt === 2) {
+            alert(`Game over! The number was ${randomNumber}. Final balance: ${balance}$`);
+        } else {
+            alert(`Incorrect! Remaining attempts: ${2 - attempt}.`);
+        }
+    }
+  }
+    askToRestartGame();
+}
+startGameLoop();
